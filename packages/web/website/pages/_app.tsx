3163103f8e99f4e31e9b4ca7c342b672a33dd2ef
Bryan Pan <bryanpan342@gmail.com>
2022-01-25 22:42:28 -0800

    feat: boilerplate next.js code in packages

---
+++ packages/web/website/pages/_app.tsx
@@ -0,0 +1,12 @@
+import {AppProps} from 'next/app';
+import React from 'react';
+import '../styles/globals.scss';
+
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <Component {...pageProps} />
+  );
+}
+
+export default MyApp;
