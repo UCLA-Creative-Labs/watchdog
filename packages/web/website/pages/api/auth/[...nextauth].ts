1b565d1ff1a9ed732b2170501af4cfc26dbea804
Bryan Pan <bryanpan342@gmail.com>
2022-01-31 08:20:33 -0800

    add next-auth secret

--- packages/web/website/pages/api/auth/[...nextauth].ts
+++ packages/web/website/pages/api/auth/[...nextauth].ts
@@ -3,6 +3,7 @@ import DiscordProvider from 'next-auth/providers/discord';
 import fetch from 'node-fetch';
 
 export default NextAuth({
+  secret: process.env.SECRET,
   providers: [
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID,
