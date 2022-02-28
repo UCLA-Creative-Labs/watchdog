1dcefcc3a6503ba75783a6175032e32a80b63f7c
Tyler Xu <xutyler937@gmail.com>
2022-02-27 17:35:27 -0800

    some fixes + reorg

--- packages/snake/helloworld/main.py
+++ packages/snake/helloworld/main.py
@@ -9,8 +9,9 @@ cred = credentials.Certificate("clauth.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-members = gc.open("All Quarters").worksheets()
-leads = gc.open("All PLead Apps").worksheets()
+member_sheet = gc.open("All Quarters").worksheets()
+lead_sheet = gc.open("All PLead Apps").worksheets()
+
 member_title = ["application_id", "applicant_name", "applicant_email", "applicant_year",
                 "applicant_major", "status", "quarter", "timestamp", "first_choice",
                 "second_choice", "third_choice"]
@@ -18,8 +19,8 @@ lead_title = ["project_id", "project_name", "project_description", "status", "le
             "lead_email", "lead_year", "open_spots"]
 
 def main():
-    store_data(members, "member-applications", db)
-    store_data(leads, "plead-applications", db)
+    store_data(member_sheet, "member-applications", db)
+    store_data(lead_sheet, "plead-applications", db)
     write_data(member_title, "member-applications", db)
     write_data(lead_title, "plead-applications", db)
 
